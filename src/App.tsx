04fb97943ca4fb92af356f8b7b64e23fd800d3f6
Fleury Escudero <fleury.ea.27@gmail.com>
2019-07-02 23:18:01 -0500

    last

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
 import * as React from 'react';
 import './App.css';
-
-import logo from './logo.svg';
+import Header from './components/Header';
+import Intro from './components/Intro';
 
 class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <div className="App">
+
+                <Header/>
+                <Intro
+                  />
+                <Intro
+                  text='Texto para intro 2'/>
+            </div>
+        );
+    }
 }
 
 export default App;
